82945c10100fd4d2677fbb0e57b8969c431f2ed3
Aleksandr Zabyshnyu <hydrostasis@ex.ua>
2017-09-19 11:54:12 +0300

    Fix twoway binding

--- src/app/apisender/apisender.component.ts
+++ src/app/apisender/apisender.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-apisender',
@@ -8,88 +7,83 @@ import { ViewChild } from '@angular/core';
   styleUrls: ['./apisender.component.css']
 })
 export class ApisenderComponent implements OnInit {
-  public appStatus = 'Ready';
-  public loginMsg: string = 'You are not logged in.';
-
-  public url:string = '';
-  public password:string = '';
-  public username:string = '';
-  public api:string = '';
-
   constructor(private http: HttpClient) { }
-
   ngOnInit() {
   }
 
-  data = {
-    JScode: '',
-    review: '',
-    token: '',
-    requestObj: ''
-  }
+  public appStatus: string = '';
+  public loginMsg: string = 'You are not logged in.';
 
-  methods = {
-    goLogin: () => {
-      if (this.url != '' && this.username != '' && this.password != '') {
-        this.loginMsg = "I'm trying to enter...";
-        this.http.post(`${this.url}/api/meta/account/${this.username}/login`, { password: this.password })
-          .subscribe(resp => {
-            if (resp['token']) {
-              this.data.token = resp['token'];
-              this.loginMsg = 'You entered.';
-            } else {
-              this.loginMsg = 'Login failed!';
-            }
-          }, (err: HttpErrorResponse) => {
-            if (err.error instanceof Error) {
-              this.data.review = 'Response error : ' + err.error.message;
-              return;
-            }
-          });
-      } else {
-        alert('Please, enter all data!');
-        return;
-      }
-    },
-    sendRequest: () => {
-      if (this.url != '' && this.api) {
-        let tempReqObj: any;
-        if (this.data.requestObj != '') {
-          try {
-            tempReqObj = JSON.parse(this.data.requestObj);
-          } catch (e) {
-            this.data.review = 'Error on your request body : ' + e.toString();
-            return;
-          }
-        } else {
-          tempReqObj = {};
-        }
+  public url: string = '';
+  public password: string = '';
+  public username: string = '';
+  public api: string = '';
 
-        this.appStatus = 'Sending...';
-        this.http.post(this.url + this.api, tempReqObj, {
-          headers: new HttpHeaders().set('X-HTTP-Method-Override', 'GET').set('Authorization', 'Bearer ' + this.data.token)
-        }).subscribe(response => {
-          this.appStatus = 'Ready!';
-          let review = JSON.stringify(response);
-          /* start running a user code */
-          try {
-            eval(this.data.JScode);
-          } catch (error) {
-            this.data.review = 'Error in your JS code : ' + error.toString();
-            return;
+  public JScode: string = '';
+  public review: string = '';
+  public token: string = '';
+  public requestObj: string = '';
+
+  goLogin() {
+    if (this.url != '' && this.username != '' && this.password != '') {
+      this.loginMsg = "I'm trying to enter...";
+      this.http.post(`${this.url}/api/meta/account/${this.username}/login`, { password: this.password })
+        .subscribe(resp => {
+          if (resp['token']) {
+            this.token = resp['token'];
+            this.loginMsg = 'You entered.';
+          } else {
+            this.loginMsg = 'Login failed!';
           }
-          this.data.review = review;
         }, (err: HttpErrorResponse) => {
           if (err.error instanceof Error) {
-            this.appStatus = 'Error!';
-            this.data.review = 'Response error : ' + err.error.message;
+            this.review = 'Response error : ' + err.error.message;
             return;
           }
         });
+    } else {
+      alert('Please, enter all data!');
+      return;
+    }
+  }
+  sendRequest() {
+    if (this.url != '' && this.api) {
+      let tempReqObj: any;
+      if (this.requestObj != '') {
+        try {
+          tempReqObj = JSON.parse(this.requestObj);
+        } catch (e) {
+          this.review = 'Error on your request body : ' + e.toString();
+          return;
+        }
       } else {
-        alert('Please, enter all data!');
-        return;
+        tempReqObj = {};
       }
+
+      this.appStatus = 'Sending...';
+      this.http.post(this.url + this.api, tempReqObj, {
+        headers: new HttpHeaders().set('X-HTTP-Method-Override', 'GET').set('Authorization', 'Bearer ' + this.token)
+      }).subscribe(response => {
+        this.appStatus = 'Ready!';
+        let review = JSON.stringify(response);
+        /* start running a user code */
+        try {
+          eval(this.JScode);
+        } catch (error) {
+          this.review = 'Error in your JS code : ' + error.toString();
+          return;
+        }
+        this.review = review;
+      }, (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          this.appStatus = 'Error!';
+          this.review = 'Response error : ' + err.error.message;
+          return;
+        }
+      });
+    } else {
+      alert('Please, enter all data!');
+      return;
     }
   }
 }
